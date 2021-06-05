1693af452d2a7616e2ac5cbf1b204e805f7a4428
Luca Martinelli <lucxmangajet@gmail.com>
2021-06-05 13:17:11 +0200

    added character selection back

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -4,19 +4,39 @@ using UnityEngine;
 
 public class Settings : SingletonBehaviour<Settings>
 {
-    private string userName;
+    [SerializeField] Texture2D[] skins;
+
+    private string username = "Greta";
     private int userSkin;
+
+    public int UserSkinID => userSkin;
+    public string Username => username;
+
+    public Texture2D[] Skins => skins;
+
+    public int ChangeSkinID(int id)
+    {
+        userSkin = (id + skins.Length) % skins.Length;
+        return userSkin;
+    }
+
+    public void SetUserName(string username)
+    {
+        this.username = username;
+    }
+
 
     protected override void Awake()
     {
-        userName = PlayerPrefs.GetString(nameof(userName));
+        base.Awake();
+        username = PlayerPrefs.GetString(nameof(username));
         userSkin = PlayerPrefs.GetInt(nameof(userSkin));
     }
 
 
     private void OnDestroy()
     {
-        PlayerPrefs.SetString(nameof(userName), userName);
+        PlayerPrefs.SetString(nameof(username), username);
         PlayerPrefs.SetInt(nameof(userSkin), userSkin);
         PlayerPrefs.Save();
     }
