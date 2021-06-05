a3d3549e8cd8ab1f35ef4718f6327bf0dda57342
LucasBoe <lucas@lumotex.de>
2021-06-05 13:58:57 +0200

    Tried to add Networking to Emoji Handler

--- Assets/Scripts/EmojiDiplayHandler.cs
+++ Assets/Scripts/EmojiDiplayHandler.cs
@@ -1,13 +1,47 @@
+using Mirror;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EmojiDiplayHandler : SingletonBehaviour<EmojiDiplayHandler>
+public class EmojiDiplayHandler : NetworkBehaviour
 {
     [SerializeField] EmojiObject prefab;
     [SerializeField] Vector3 offset;
+    [SerializeField] List<Sprite> emojis;
+
+    private static EmojiDiplayHandler instance;
+
+    public static EmojiDiplayHandler Instance => instance;
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Debug.LogError($"Spawned Second Instance of {GetType()}, destroying");
+            Destroy(gameObject);
+        }
+    }
+
     public void SpawnEmoji(Vector3 playerPosition, Sprite emojiSprite)
     {
+        int emjoiIndex = emojis.IndexOf(emojiSprite);
+        CMD_SpawnEmoji(playerPosition, emjoiIndex);
+    }
+
+    [Command]
+    private void CMD_SpawnEmoji(Vector3 playerPosition, int emojiSpriteIndex)
+    {
+        RPC_SpawnEmoji(playerPosition, emojiSpriteIndex);
+    }
+
+    [ClientRpc]
+    private void RPC_SpawnEmoji(Vector3 playerPosition, int emojiSpriteIndex)
+    {
+        Sprite emojiSprite = emojis[emojiSpriteIndex];
         EmojiObject emojiObject = Instantiate(prefab, playerPosition + offset, Quaternion.identity, transform);
         emojiObject.SetSprite(emojiSprite);
     }
