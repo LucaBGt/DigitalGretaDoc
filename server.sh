397138e094dfe6017d86009baed6eb768373e959
Luca Martinelli <lucxmangajet@gmail.com>
2021-06-16 21:40:39 +0200

    server test

--- server.sh
+++ server.sh
@@ -5,3 +5,17 @@ cd ${WORKSPACE}
 ${JENKINS_HOME}/2020.3.11f1/Editor/Unity -batchmode -manualLicenseFile ${JENKINS_HOME}/2020.3.11f1/Unity_lic.ulf
 
 ${JENKINS_HOME}/2020.3.11f1/Editor/Unity -batchmode -nographics -quit -executeMethod JenkinsBuild.BuildLinuxServer "${WORKSPACE}\\Build_Server"
+
+echo "Build Done, stopping server"
+
+kill $(ps aux | grep '/root/GretaServer/' | awk '{print $2}')
+
+echo "Copying server"
+
+cp -a "${WORKSPACE}\\Build_Server\\." /root/GretaServer/
+
+echo "Restarting Server"
+
+./root/GretaServer/Server/Server.x86_64 -batchmode -nographics
+
+echo "Done"
