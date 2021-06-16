d5d535463a745cbebc9ea2557240fe190da71ee6
Luca Martinelli <lucxmangajet@gmail.com>
2021-06-16 22:29:01 +0200

    fixed, changing back

--- server.sh
+++ server.sh
@@ -1,9 +1,9 @@
 
 cd ${WORKSPACE}
 
-#${JENKINS_HOME}/2020.3.11f1/Editor/Unity -batchmode -manualLicenseFile ${JENKINS_HOME}/2020.3.11f1/Unity_lic.ulf
+${JENKINS_HOME}/2020.3.11f1/Editor/Unity -batchmode -manualLicenseFile ${JENKINS_HOME}/2020.3.11f1/Unity_lic.ulf
 
-#${JENKINS_HOME}/2020.3.11f1/Editor/Unity -batchmode -nographics -quit -executeMethod JenkinsBuild.BuildLinuxServer "${WORKSPACE}\\Build_Server"
+${JENKINS_HOME}/2020.3.11f1/Editor/Unity -batchmode -nographics -quit -executeMethod JenkinsBuild.BuildLinuxServer "${WORKSPACE}\\Build_Server"
 
 echo "Build Done, killing server"
 
