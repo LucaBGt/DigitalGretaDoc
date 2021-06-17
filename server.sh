e6ca2048f905c8797c006a482095173d714b5cba
Luca Martinelli <lucxmangajet@gmail.com>
2021-06-17 11:27:04 +0200

    setup jenkins for webgl build

--- server.sh
+++ server.sh
@@ -3,9 +3,19 @@ cd ${WORKSPACE}
 
 ${JENKINS_HOME}/2020.3.11f1/Editor/Unity -batchmode -manualLicenseFile ${JENKINS_HOME}/2020.3.11f1/Unity_lic.ulf
 
-${JENKINS_HOME}/2020.3.11f1/Editor/Unity -batchmode -nographics -quit -executeMethod JenkinsBuild.BuildLinuxServer "${WORKSPACE}\\Build_Server" || exit 1
+${JENKINS_HOME}/2020.3.11f1/Editor/Unity -batchmode -nographics -quit -executeMethod JenkinsBuild.BuildLinuxServer "${WORKSPACE}/Build_Server" || exit 1
+echo "Server Build Done"
 
-echo "Build Done, killing server"
+sleep 3 
+
+${JENKINS_HOME}/2020.3.11f1/Editor/Unity -batchmode -nographics -quit -executeMethod JenkinsBuild.BuildWebGL "${WORKSPACE}/Build_WebGL" || exit 1
+echo "WebGL Build Done"
+
+echo "Copying WebGL build to local site location"
+cp -r ${WORKSPACE}/Build_WebGL/. /var/www/html/Gretaland/ || exit 1
+
+#replace and restart server
+echo "killing server"
 
 pkill Server.x86_64
 
