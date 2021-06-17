98bd6d69076631412b23dd897cf17f7b2b65728f
Luca Martinelli <lucxmangajet@gmail.com>
2021-06-17 12:51:11 +0200

    fixed shell build step

--- server.sh
+++ server.sh
@@ -1,18 +1,19 @@
 
+set -e
 cd ${WORKSPACE}
 
 ${JENKINS_HOME}/2020.3.11f1/Editor/Unity -batchmode -manualLicenseFile ${JENKINS_HOME}/2020.3.11f1/Unity_lic.ulf
 
-${JENKINS_HOME}/2020.3.11f1/Editor/Unity -batchmode -nographics -quit -executeMethod JenkinsBuild.BuildLinuxServer "${WORKSPACE}/Build_Server" || exit 1
+${JENKINS_HOME}/2020.3.11f1/Editor/Unity -batchmode -nographics -quit -executeMethod JenkinsBuild.BuildLinuxServer "${WORKSPACE}/Build_Server"
 echo "Server Build Done"
 
 sleep 3 
 
-${JENKINS_HOME}/2020.3.11f1/Editor/Unity -batchmode -nographics -quit -executeMethod JenkinsBuild.BuildWebGL "${WORKSPACE}/Build_WebGL" || exit 1
+${JENKINS_HOME}/2020.3.11f1/Editor/Unity -batchmode -nographics -quit -executeMethod JenkinsBuild.BuildWebGL "${WORKSPACE}/Build_WebGL"
 echo "WebGL Build Done"
 
 echo "Copying WebGL build to local site location"
-cp -r ${WORKSPACE}/Build_WebGL/. /var/www/html/Gretaland/ || exit 1
+cp -r ${WORKSPACE}/Build_WebGL/. /var/www/html/Gretaland/
 
 #replace and restart server
 echo "killing server"
@@ -23,10 +24,10 @@ sleep 3
 
 echo "Copying server"
 
-cp -r ${WORKSPACE}/Build_Server/. /root/GretaServer/ || exit 1
+cp -r ${WORKSPACE}/Build_Server/. /root/GretaServer/ 
 
 echo "Restarting Server"
 
-daemonize -E BUILD_ID=dontKillMe /root/GretaServer/run.sh || exit 1
+daemonize -E BUILD_ID=dontKillMe /root/GretaServer/run.sh
 
 echo "Done"
