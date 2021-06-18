a717d09e546934f073e71dd60fef47db95b2e39e
Luca Martinelli <lucxmangajet@gmail.com>
2021-06-18 14:53:31 +0200

    ...

--- server.sh
+++ server.sh
@@ -6,14 +6,14 @@ cd ${WORKSPACE}
 UNITY_PATH=/root/Unity/Hub/Editor/2020.3.12f1/Editor/Unity
 UNITY_LICENSE=/root/Unity/Unity_lic.ulf
 
-UNITY_PATH -batchmode -manualLicenseFile UNITY_LICENSE || echo "Unity Licensing Failed (Caused when Already Setup)"
+$UNITY_PATH -batchmode -manualLicenseFile $UNITY_LICENSE || echo "Unity Licensing Failed (Caused when Already Setup)"
 
-UNITY_PATH -batchmode -nographics -quit -executeMethod JenkinsBuild.BuildLinuxServer "${WORKSPACE}/Build_Server"
+$UNITY_PATH -batchmode -nographics -quit -executeMethod JenkinsBuild.BuildLinuxServer "${WORKSPACE}/Build_Server"
 echo "Server Build Done"
 
 sleep 3 
 
-UNITY_PATH -batchmode -nographics -quit -executeMethod JenkinsBuild.BuildWebGL "${WORKSPACE}/Build_WebGL"
+$UNITY_PATH -batchmode -nographics -quit -executeMethod JenkinsBuild.BuildWebGL "${WORKSPACE}/Build_WebGL"
 echo "WebGL Build Done"
 
 echo "Copying WebGL build to local site location"
