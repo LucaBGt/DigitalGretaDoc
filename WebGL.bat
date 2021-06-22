760f5560c1df563c703d6ec2ba948259d12681eb
Luca Martinelli <lucxmangajet@gmail.com>
2021-06-22 17:39:39 +0200

    ..

--- WebGL.bat
+++ WebGL.bat
@@ -3,7 +3,7 @@ echo "Starting WebGL.bat"
 
 cd %WORKSPACE%
 
-set UNITY_PATH = C:\Program Files\Unity\Hub\Editor\2020.3.5f1\Editor\Unity
+set UNITY_PATH=C:\Program Files\Unity\Hub\Editor\2020.3.5f1\Editor\Unity
 
 %UNITY_PATH% -batchmode -nographics -quit -executeMethod JenkinsBuild.BuildWebGL "%WORKSPACE%/Build_WebGL/"
 echo "WebGL Build Done"
