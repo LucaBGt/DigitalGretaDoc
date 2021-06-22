8680f63e14a9122d840d83d082a0dec598d640ef
Luca Martinelli <lucxmangajet@gmail.com>
2021-06-22 15:45:15 +0200

    setup webGL shell for windows

--- WebGL.sh
+++ WebGL.sh
@@ -3,17 +3,9 @@ echo "Starting WebGL.sh"
 set -e
 cd ${WORKSPACE}
 
-UNITY_PATH=/root/Unity/Hub/Editor/2020.3.12f1/Editor/Unity
-UNITY_LICENSE=/root/Unity/Unity_lic.ulf
-
-$UNITY_PATH -batchmode -manualLicenseFile $UNITY_LICENSE || echo "Unity Licensing Failed (Caused when Already Setup)"
+UNITY_PATH=C:\Program Files\Unity\Hub\Editor\2020.3.5f1\Editor\Unity
 
 #WEBGL STUFF 
 $UNITY_PATH -batchmode -nographics -quit -executeMethod JenkinsBuild.BuildWebGL "${WORKSPACE}/Build_WebGL/"
 echo "WebGL Build Done"
 
-echo "Copying WebGL build to local site location"
-cp -r ${WORKSPACE}/Build_WebGL/. /var/www/html/Gretaland/
-
-
-echo "Done"
