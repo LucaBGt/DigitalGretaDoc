2c4ae20434f308c445c5ab5b24cb067411ec8565
LucasBoe <lucas@lumotex.de>
2021-08-21 09:51:56 +0200

    Added new speechbubble prefabs

--- Assets/Scripts/UI/EmojiObject.cs
+++ Assets/Scripts/UI/EmojiObject.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class EmojiObject : MonoBehaviour
 {
+    [SerializeField] TMP_Text textDisplay;
     [SerializeField] float duration = 3;
 
-    private void Start()
+    public void Init(string text)
     {
+        textDisplay.text = text;
         Destroy(gameObject, duration);
     }
 }
